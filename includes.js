30ea466ad058863983e3621e958a376fcb17dfc0
Andrey Gubanov <bookfast.org@gmail.com>
2015-12-16 15:01:55 +0200

    Handle brackets in pattern correctly

--- includes.js
+++ includes.js
@@ -1,76 +1,75 @@
-( function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define(factory);
-    } else {
-        window.includes = factory();
-    }
+(function(root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		define(factory);
+	} else {
+		window.includes = factory();
+	}
 }(this, function() {
 	"use strict";
-	var includes = function( o ) {
-		o = o || {};
-		pattern = o.pattern || '{{include "FILE_NAME"}}'; // /\{\{include\s+"(\S+)"\}\}/;
-		path = o.path || '';
-		suffix = o.suffix || '';
-		
-		pattern = typeof pattern == 'string' 
-			? new RegExp( pattern.replace( /\s+/, '\\s+' )
-				.replace( 'FILE_NAME', '(\\S+)' ) ) 
-			: pattern;
+	var includes = function(o) {
+			o = o || {};
+			pattern = o.pattern || '{{include "FILE_NAME"}}'; // /\{\{include\s+"(\S+)"\}\}/;
+			path = o.path || '';
+			suffix = o.suffix || '';
 
-		all = document.querySelectorAll( '*' );
-		for( i = 0; i < all.length; i++ ) {
-			allItem = all[ i ];
-			if( allItem.tagName == 'SCRIPT' ) continue;
-			childNodes = allItem.childNodes;
-			for( j = 0; j < childNodes.length; j++ ) {
-				node = childNodes[ j ];
+			pattern = typeof pattern == 'string' ? new RegExp(pattern.replace(/\s+/, '\\s+')
+				.replace(/\(/g, '\\(')
+				.replace(/\)/g, '\\)')
+				.replace('FILE_NAME', '(\\S+)')) : pattern;
+			window.pattern = pattern;
 
-				if( node.nodeType === 3 && pattern.test( node.nodeValue ) ) {
-					count = 0;
-					html = node.nodeValue;
-					
-					while( pattern.test( html ) ) {
-						html = html.replace( pattern, function( $0, $1 ) {
-							if( cache[ $1 ]  ) {
-								return cache[ $1 ];
-							} else {
-								request = new XMLHttpRequest();
-								request.open( 'GET', path + $1 + suffix, false );
-								request.send();
-								return cache[ $1 ] = request.responseText;
+			all = document.querySelectorAll('*');
+			for (i = 0; i < all.length; i++) {
+				allItem = all[i];
+				if (allItem.tagName == 'SCRIPT') continue;
+				childNodes = allItem.childNodes;
+				for (j = 0; j < childNodes.length; j++) {
+					node = childNodes[j];
+
+					if (node.nodeType === 3 && pattern.test(node.nodeValue)) {
+						count = 0;
+						html = node.nodeValue;
+
+						while (pattern.test(html)) {
+							html = html.replace(pattern, function($0, $1) {
+								if (cache[$1]) {
+									return cache[$1];
+								} else {
+									request = new XMLHttpRequest();
+									request.open('GET', path + $1 + suffix, false);
+									request.send();
+									return cache[$1] = request.responseText;
+								}
+							});
+
+							count++;
+
+							if (count > 1e4) {
+								html = '<b>Include Error:</b> Circular Reference';
+								break;
 							}
-						});
-			
-						count++;
-						
-						if( count > 1e4 ) {
-							html = '<b>Include Error:</b> Circular Reference';
-							break;
 						}
+
+						node.nextSibling ? node.nextSibling.insertAdjacentHTML('beforebegin', html) : node.parentNode.insertAdjacentHTML('beforeend', html);
+
+						allItem.removeChild(node);
+						j--;
 					}
-					
-					node.nextSibling
-						? node.nextSibling.insertAdjacentHTML( 'beforebegin', html ) 
-						: node.parentNode.insertAdjacentHTML( 'beforeend', html );
-										
-					allItem.removeChild( node );
-					j--;
 				}
 			}
-		}
-	},
-	pattern,
-	path,
-	suffix,
-	cache = {},
-	html,
-	all,
-	allItem,
-	node,
-	childNodes,
-	request,
-	i, j,
-	count;
-	
+		},
+		pattern,
+		path,
+		suffix,
+		cache = {},
+		html,
+		all,
+		allItem,
+		node,
+		childNodes,
+		request,
+		i, j,
+		count;
+
 	return includes;
 }));
